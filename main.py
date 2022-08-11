5396b6fd3da6f44bbc3e22bb51d0db257009710f
Brian Rojas Chale <Brarojasc@gmail.com>
2022-08-10 19:22:47 -0600

    saving in blocks

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
+from itertools import count
 import utils
 import time
 import csv
-import os
+import resource
+from os import getenv
+from os import scandir
+from os import getcwd
+from os import remove
+from pathlib import Path
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from glob import glob
+from multiprocessing import Pool
 
 from polyglot.detect import Detector
 from polyglot.detect.base import logger as polyglot_logger
@@ -14,39 +21,84 @@ from polyglot.detect.base import UnknownLanguage
 
 polyglot_logger.setLevel("ERROR")
 load_dotenv()
-ENVIRONMENT = os.getenv("ENVIRONMENT")
-REMOVE_FILES_AT_END = True if (os.getenv("REMOVE_FILES_AT_END") == "true") else False
+ENVIRONMENT = getenv("ENVIRONMENT")
+REMOVE_FILES_AT_END = True if (getenv("REMOVE_FILES_AT_END") == "true") else False
+MAX_ROWS = int(getenv("MAX_ROWS"))
+COUNTRIES = []
+LANGUAGES = []
+LOGFILES = []
+DATA = []
 
-def incrementCountry(countriesTable, code):
-    country = countriesTable.find_one({"code": code})
-    if(country == None):
-        countriesTable.insert_one({"code": code, "name": "", "quantity": 1})
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    return free_memory
+
+def memory_limit():
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (get_memory() * 1024 / 2, hard))
+
+def incrementCountry(code):
+    index = next((i for i, item in enumerate(COUNTRIES) if item["code"] == code), None)
+    if(index == None):
+        COUNTRIES.append({'code': code, 'name': '', 'quantity': 1})
     else:
-        quantity = country["quantity"]
-        quantity = quantity + 1
-        countriesTable.update_one({"_id": country["_id"]}, { "$set": { "quantity": quantity } })
+        COUNTRIES[index]['quantity'] += 1
 
-def incrementLanguage(languagesTable, lang, code):
-    language = languagesTable.find_one({"code": code})
-    if(language == None):
-        languagesTable.insert_one({"name": lang, "code": code, "quantity": 1})
+def incrementLanguage(lang, code):
+    index = next((i for i, item in enumerate(LANGUAGES) if item["code"] == code), None)
+    if(index == None):
+        LANGUAGES.append({'code': code, 'name': lang, 'quantity': 1})
     else:
-        quantity = language["quantity"]
-        quantity = quantity + 1
-        languagesTable.update_one({"_id": language['_id']}, { "$set": {"quantity": quantity } })
+        LANGUAGES[index]['quantity'] += 1
 
-def log(filesTable, file, rows, time):
-    filesTable.insert_one({'file': file, 'rows': rows, 'time': time})
+def log(file, rows, time):
+    LOGFILES.append({'file': file, 'rows': rows, 'time': time})
+    
+def insertData(date, ip, domain, country, sender, subject, language, confidence):
+    DATA.append({'date': date, 'ip': ip, 'domain': domain, 'country': country, 'sender': sender, 'subject':subject, 'language': language, 'confidence': confidence, 'translate_subject': '' })
 
-def insertData(dataTable, date, ip, domain, country, sender, subject, language, confidence):
-    dataTable.insert_one({'date': date, 'ip': ip, 'domain': domain, 'country': country, 'sender': sender, 'subject':subject, 'language': language, 'confidence': confidence, 'translate_subject': '' })
+def cleanLists():
+    COUNTRIES.clear()
+    LANGUAGES.clear()
+    LOGFILES.clear()
+    DATA.clear()
+
+def save_processed_data(db):
+    dataTable = db.processed_data
+    dataTable.insert_many(DATA)
+    DATA.clear()
 
 def main():
-    list_csvs = glob("./Feeds/**/*.csv",recursive = True)
-    for file in list_csvs:
-        process(file)
-        if(REMOVE_FILES_AT_END):
-            os.remove(file)
+    subfolders = [f.path for f in scandir(getcwd() +'/Feeds/') if f.is_dir()]
+
+    if(ENVIRONMENT == "dev"):
+        client = MongoClient()
+    elif(ENVIRONMENT == "prod"):
+        client = MongoClient(username="root", password="12345", authSource="admin")
+
+    for folder in subfolders:
+        f = folder.split("/")[6]
+        list_csvs = glob(f"./Feeds/{f}/*.csv", recursive = True)
+        db = client[f]        
+        for file in list_csvs:
+            process(file, db)
+            if(REMOVE_FILES_AT_END):
+                remove(file)
+        filesTable = db.log_files
+        countriesTable = db.countries
+        languagesTable = db.languages
+
+        countriesTable.insert_many(COUNTRIES)
+        languagesTable.insert_many(LANGUAGES)
+        filesTable.insert_many(LOGFILES)
+        save_processed_data(db)
+        cleanLists()
+    client.close()
 
 """ HEADERS
 # CSIRTAmericas Observation time [0],
@@ -61,18 +113,7 @@ def main():
 # Taxonomy [14] = Taxonomic Category from CSIRTAMERICAS,
 # Provider [15]
 """
-def process(file):
-    folders = file.split("/")
-    if(ENVIRONMENT == "dev"):
-        client = MongoClient()
-    elif(ENVIRONMENT == "prod"):
-        client = MongoClient(username="root", password="12345", authSource="admin")
-    db = client[folders[2]]
-    countriesTable = db.countries
-    languagesTable = db.languages
-    dataTable = db.processed_data
-    filesTable = db.log_files
-
+def process(file, db):
     with open(file) as csv_file:
         start_time_file = time.time()
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -83,22 +124,24 @@ def process(file):
                     line_count += 1
                 else:
                     #print(f'\t FROM: {row[8]} / IP: {row[2]} / SUBJECT: {row[9]}')
-                    db = client.spam_account if (file.find("spam_account") != -1) else client.spam_relay4
                     clean_date      = row[1]
                     clean_ip        = row[2]
                     clean_country   = row[5]
                     clean_domain    = row[7] if (len(row) == 16) else "" 
                     clean_sender    = row[8] if (len(row) == 16) else ""
                     clean_subject   = utils.remove_bad_chars(row[9]) if (len(row) == 16) else utils.remove_bad_chars(row[7])
-                    incrementCountry(countriesTable, clean_country)
+                    incrementCountry(clean_country)
                     detector = Detector(clean_subject, quiet=True)
-                    incrementLanguage(languagesTable, detector.language.name, detector.language.code)
-                    insertData(dataTable, clean_date, clean_ip, clean_domain, clean_country, clean_sender, clean_subject, detector.language.name, detector.language.confidence)
+                    incrementLanguage(detector.language.name, detector.language.code)
+                    insertData(clean_date, clean_ip, clean_domain, clean_country, clean_sender, clean_subject, detector.language.name, detector.language.confidence)
                     line_count += 1
+                    if(len(DATA) == MAX_ROWS):
+                        save_processed_data(db)
         except csv.Error:
             print(f"An exception occurred at line {csv_reader.line_num} in file {file}")
         file_time = (time.time() - start_time_file) * 1000
-        log(filesTable, file, line_count, file_time)
-        client.close()
-
+        log(file, line_count, file_time)
+        
+start_time = time.time()
 main()
+print("--- %s seconds ---" % (time.time() - start_time))
